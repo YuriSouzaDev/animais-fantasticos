3f3adcded838eccafd6b1696ab2a91c0ff85100b
YuriSouzaDev <yurisouza.developer@gmail.com>
2023-09-25 22:07:11 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
+import Tabnav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
