fa7b83efe049b6e7cddc41cd0e2cd0e6d4c54457
YuriSouzaDev <yurisouza.developer@gmail.com>
2023-09-25 21:54:17 -0300

    Acordion refactor in class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,30 +1,29 @@
-export default function initAccordion() {
-  // função de abrir perguntas faq
-  const accordionList = document.querySelectorAll(
-    "[data-anime='accordion'] dt"
-  );
-  const activeClass = "ativo";
-
-  // verifica se o elemento existe para ativar a função
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    // adiciona evento de click nos "dt"
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
-    });
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
   }
 
-  function activeAccordion() {
-    // faz um loop para remover a classe ao clicar em outro elemento
-    accordionList.forEach((e) => {
-      e.classList.remove(activeClass);
-      e.nextElementSibling.classList.remove(activeClass);
-    });
-    //  "this" se refere ao elemento "dt"
-    this.classList.toggle(activeClass);
+  toggleAccordion(item) {
+    //se refere ao elemento "dt"
+    item.classList.toggle(this.activeClass);
     // se refere ao "dd"
-    this.nextElementSibling.classList.toggle(activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  // Adiciona os eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      // adiciona evento de click nos "dt"
+      item.addEventListener("click", () => this.toggleAccordion(item));
+    });
+  }
+  // Iniciar função
+  init() {
+    // ativar primeiro item
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
