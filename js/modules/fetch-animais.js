26fcdea29da362b24f0293134e011fa827600724
YuriSouzaDev <yurisouza.developer@gmail.com>
2023-10-01 01:08:12 -0300

    animais-numeros refactor

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,29 +1,31 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
-export default function initiFecthAnimais() {
+export default function FecthAnimais() {
+  function createAnimal(animal) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
+
   async function fetchAnimais(url) {
     try {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
       const numerosGrid = document.querySelector(".numeros-grid");
-
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();
-    } catch (error) {
-      console.log(error);
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo"
+      );
+      animaNumeros.init();
+    } catch (erro) {
+      console.log(erro);
     }
-  }
-
-  function createAnimal(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-
-    div.innerHTML = ` <h3>${animal.especie}</h3>
-                    <span data-numeros>${animal.total}</span>`;
-    return div;
   }
 
   fetchAnimais("./animais-api.json");
